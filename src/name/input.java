a4d2bdbca59f70ac878adb9151ad2f830bf0caf8
wangfuzhu <31799655+wangfuzhu@users.noreply.github.com>
2018-02-24 16:13:16 +0300

    всё
    Signed-off-by: wangfuzhu <31799655+wangfuzhu@users.noreply.github.com>

--- src/name/input.java
+++ src/name/input.java
@@ -1,4 +1,7 @@
 package name;
+
+import java.text.DecimalFormat;
+
 public class input {
     // information
     private int id;// id
@@ -37,6 +40,51 @@ public class input {
     }
     public int getSma() {
         return smallPrice;
+    }
+    public String nameOld(){
+        String nam = name;
+        System.out.println("имя:"+ nam);
+        return name;
+    }
+    public String nameN(String nameNew){
+        String namN = "";
+        if (nameNew == "не изменение"){
+            System.out.println("новый имя:"+ name);
+            return namN;
+        }else {
+            System.out.println("новый имя:"+ nameNew);
+            return namN;
+        }
+    }
+    public int nNumber(){
+        int nNum = number;
+        System.out.println("количество:"+ nNum);
+        return number;
+    }
+    public int numberCha(int nu){
+        int sum = number + nu;
+        if (sum >= 0){
+            System.out.println("сейчас количество:"+sum);
+            return sum;
+        }else{
+            System.out.println("нету");
+            return sum;
+        }
+    }
+    public String moneySum(){
+        System.out.println(bigPrice+"рублей"+smallPrice+"копеек");
+        return "";
+    }
+    public String money(double moneyCh){
+        double big = bigPrice;
+        double small = smallPrice;
+        double moneyN = big + small / 100;
+        double moneyAll = moneyN + moneyCh;
+        double sma = moneyAll*100;
+        int moneyS = (int)sma - (int) moneyAll *100;
+        DecimalFormat tow  = new DecimalFormat("######0");
+        System.out.println("новая цена:"+tow.format(moneyAll)+"рублей"+moneyS+"копеек");
+        return "";
     }
 }
 
--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 import name.input;
+import java.lang.String;
 
 public class Test {
     private static Map<Integer,input> goods = new HashMap<Integer,input>();
@@ -18,14 +19,27 @@ public class Test {
         printTable("3",name3,"128","60","34");
         printTable("4",name4,"170","22","25");
         goods.put(1,new input(1,name1,8,55,49));
-        goods.put(1,new input(2,name2,12,50,11));
-        goods.put(1,new input(3,name3,34,128,60));
-        goods.put(1,new input(4,name4,25,170,22));
+        goods.put(2,new input(2,name2,12,50,11));
+        goods.put(3,new input(3,name3,34,128,60));
+        goods.put(4,new input(4,name4,25,170,22));
         Scanner inside = new Scanner(System.in);
         System.out.println("номер:");
         int unmber = inside.nextInt();
         input market = goods.get(unmber);
-        
+        market.nameOld();
+        System.out.println("изменение имя:");
+        String nameNew = inside.next();
+        market.nameN(nameNew);
+        market.nNumber();
+        int nu = inside.nextInt();
+        market.numberCha(nu);
+        market.moneySum();
+        double moneyCh = inside.nextDouble();
+        market.money(moneyCh);
+        System.out.println("Последняя версия");
+        market.nameN(nameNew);
+        market.numberCha(nu);
+        market.money(moneyCh);
     }
     private static void printTable(String row1,String row2,String row3,String row4,String row5) {
         System.out.print(row1);
